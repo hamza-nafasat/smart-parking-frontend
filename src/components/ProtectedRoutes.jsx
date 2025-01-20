4e1af462c0297a4ff9b323db54ae5117f1d84c76
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-20 19:16:46 +0500

    add protected routes

---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProtectedRoutes = ({ requiresAuth = false, redirect = "/login", role }) => {
+  const { user } = useSelector((state) => state.auth);
+  if (requiresAuth && !user) return <Navigate to={redirect} replace />;
+  if (requiresAuth && user?.role != role) return <Navigate to={`/${user?.role}`} replace />;
+  if (!requiresAuth && user) return <Navigate to={`/${user?.role}`} replace />;
+  return <Outlet />;
+};
+
+export default ProtectedRoutes;
