5a6c661d2f064a3414a25bfe04b067bddce0a8ce
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-20 19:17:40 +0500

    some other changes and fix protected routes logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useCallback, useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Loader from "./components/shared/small/Loader";
@@ -11,6 +11,12 @@ import PaymentFail from "./pages/user/dashboard/components/PaymentFail";
 import UserViewSlip from "./pages/user/bookingSummary/components/ViewSlip";
 import UserProfile from "./pages/user/setting/Profile";
 import BookingSlots from "./pages/user/dashboard/components/BookingSlots";
+import { Toaster } from "react-hot-toast";
+import { useCheckLoginMutation } from "./redux/apis/authApis";
+import { useDispatch } from "react-redux";
+import { userExist, userNotExist } from "./redux/slices/authSlice";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
 const LandingPage = lazy(() => import("./pages/landing/LandingPage"));
 // auth imports
 const SignIn = lazy(() => import("./pages/auth/SignIn"));
@@ -26,7 +32,6 @@ const Wallet = lazy(() => import("./pages/admin/wallet/Wallet"));
 const ViewSlipAdmin = lazy(() => import("./pages/admin/dashboard/components/ViewSlip"));
 const WalletTransactionsDetail = lazy(() => import("./pages/admin/wallet/components/WalletTransactonsDetails"));
 const BuildingView = lazy(() => import("./pages/admin/buildingInfo/components/BuildingView"));
-
 // Manager imports
 const Manager = lazy(() => import("./pages/manager/index"));
 const ManagerDashboard = lazy(() => import("./pages/manager/dashboard/ManagerDashboard"));
@@ -35,7 +40,6 @@ const ManagerFloorView = lazy(() => import("./pages/manager/buildingInfo/compone
 const ManagerParkingSummary = lazy(() => import("./pages/manager/parkingSummary/ParkingSummary"));
 const ManagerWallet = lazy(() => import("./pages/manager/wallet/Wallet"));
 const ManagerProfile = lazy(() => import("./pages/manager/settings/Profile"));
-
 // User imports
 const User = lazy(() => import("./pages/user/index"));
 const UserDashboard = lazy(() => import("./pages/user/dashboard/UserDashboard"));
@@ -44,52 +48,85 @@ const UserBuildingInfo = lazy(() => import("./pages/user/buildingInfo/BuildingIn
 const AddParkingSpace = lazy(() => import("./pages/manager/addParkingSpace/AddParkingSpace"));
 
 function App() {
-  const loader = <Loader />;
-  return (
-    <Router>
-      <Suspense fallback={loader}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="login" element={<SignIn />} />
-          <Route path="register" element={<SignUp />} />
-          <Route path="forget-password" element={<ForgetPassword />} />
-          <Route path="reset-password" element={<ResetPassword />} />
-          <Route path="/admin" element={<Admin />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="building-info" element={<BuildingInfo />} />
-            <Route path="building-view/:id" element={<BuildingView />} />
-            <Route path="floor-view/:id" element={<FloorView />} />
-            <Route path="parking-summary" element={<ParkingSummary />} />
-            <Route path="wallet" element={<Wallet />} />
-            <Route path="wallet-transactions-detail" element={<WalletTransactionsDetail />} />
-            <Route path="view-slip/:id" element={<ViewSlipAdmin />} />
-          </Route>
-          <Route path="/manager" element={<Manager />}>
-            <Route index element={<ManagerDashboard />} />
-            <Route path="building-info" element={<ManagerBuildingInfo />} />
-            <Route path="floor-view/:id" element={<ManagerFloorView />} />
-            <Route path="parking-summary" element={<ManagerParkingSummary />} />
-            <Route path="wallet" element={<ManagerWallet />} />
-            <Route path="profile" element={<ManagerProfile />} />
-            <Route path="profile" element={<ManagerProfile />} />
-            <Route path="add-parking-space" element={<AddParkingSpace />} />
-          </Route>
-          <Route path="/user" element={<User />}>
-            <Route index element={<UserDashboard />} />
-            <Route path="building-info" element={<UserBuildingInfo />} />
-            <Route path="booking-summary" element={<BookingSummary />} />
-            <Route path="view-slip" element={<UserViewSlip />} />
+  const dispatch = useDispatch();
+  const [checkLogin] = useCheckLoginMutation();
+  const [isLoading, setIsLoading] = useState(true);
 
-            <Route path="booking" element={<Booking />} />
-            <Route path="confirm-booking" element={<ConfirmBooking />} />
-            <Route path="payment-success" element={<PaymentSuccess />} />
-            <Route path="payment-failed" element={<PaymentFail />} />
-            <Route path="booking-slot" element={<BookingSlots />} />
-            <Route path="profile" element={<UserProfile />} />
-          </Route>
-        </Routes>
+  const checkUserLogin = useCallback(async () => {
+    try {
+      const res = await checkLogin().unwrap();
+      if (res) dispatch(userExist(res?.data));
+    } catch (error) {
+      console.log("Error while checking user login", error);
+      dispatch(userNotExist());
+    } finally {
+      setIsLoading(false);
+    }
+  }, [checkLogin, dispatch]);
+
+  useEffect(() => {
+    checkUserLogin();
+  }, [checkUserLogin]);
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/forget-password" element={<ForgetPassword />} />
+              <Route path="/reset-password/:token" element={<ResetPassword />} />
+            </Route>
+
+            <Route element={<ProtectedRoutes requiresAuth role={"admin"} />}>
+              <Route path="/admin" element={<Admin />}>
+                <Route index element={<AdminDashboard />} />
+                <Route path="building-info" element={<BuildingInfo />} />
+                <Route path="building-view/:id" element={<BuildingView />} />
+                <Route path="floor-view/:id" element={<FloorView />} />
+                <Route path="parking-summary" element={<ParkingSummary />} />
+                <Route path="wallet" element={<Wallet />} />
+                <Route path="wallet-transactions-detail" element={<WalletTransactionsDetail />} />
+                <Route path="view-slip/:id" element={<ViewSlipAdmin />} />
+              </Route>{" "}
+            </Route>
+
+            <Route element={<ProtectedRoutes requiresAuth role={"manager"} />}>
+              <Route path="/manager" element={<Manager />}>
+                <Route index element={<ManagerDashboard />} />
+                <Route path="building-info" element={<ManagerBuildingInfo />} />
+                <Route path="floor-view/:id" element={<ManagerFloorView />} />
+                <Route path="parking-summary" element={<ManagerParkingSummary />} />
+                <Route path="wallet" element={<ManagerWallet />} />
+                <Route path="profile" element={<ManagerProfile />} />
+                <Route path="profile" element={<ManagerProfile />} />
+                <Route path="add-parking-space" element={<AddParkingSpace />} />
+              </Route>{" "}
+            </Route>
+
+            <Route element={<ProtectedRoutes requiresAuth role={"user"} />}>
+              <Route path="/user" element={<User />}>
+                <Route index element={<UserDashboard />} />
+                <Route path="building-info" element={<UserBuildingInfo />} />
+                <Route path="booking-summary" element={<BookingSummary />} />
+                <Route path="view-slip" element={<UserViewSlip />} />
+                <Route path="booking" element={<Booking />} />
+                <Route path="confirm-booking" element={<ConfirmBooking />} />
+                <Route path="payment-success" element={<PaymentSuccess />} />
+                <Route path="payment-failed" element={<PaymentFail />} />
+                <Route path="booking-slot" element={<BookingSlots />} />
+                <Route path="profile" element={<UserProfile />} />
+              </Route>
+            </Route>
+          </Routes>
+        )}
       </Suspense>
-    </Router>
+      <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
+    </BrowserRouter>
   );
 }
 
--- src/components/shared/small/Button.jsx
+++ src/components/shared/small/Button.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-
-const Button = ({ text, bg, border, height, width, color, ...rest }) => {
+/* eslint-disable react/prop-types */
+const Button = ({ text, bg, height, width, disabled = false, color, ...rest }) => {
   return (
     <button
       className={`grid place-items-center capitalize transition-all duration-300 text-sm md:text-base font-semibold ${
         color ? color : "text-white"
-      } ${width ? width : "w-full sm:w-auto"} ${
-        bg ? bg : "bg-primary hover:bg-transparent hover:text-primary"
-      } ${height ? height : "h-[41px]"} border-2 border-primary rounded-md`}
+      } ${width ? width : "w-full sm:w-auto"} ${bg ? bg : "bg-primary hover:bg-transparent hover:text-primary"} ${
+        height ? height : "h-[41px]"
+      } border-2 border-primary rounded-md ${disabled && "cursor-not-allowed opacity-30"}`}
       {...rest}
     >
       {text}
