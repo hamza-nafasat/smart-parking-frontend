e426be3189822d9883965ae71cc4890a53172e2a
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-21 12:08:43 +0500

    integrate forget password api

--- src/pages/auth/ForgetPassword.jsx
+++ src/pages/auth/ForgetPassword.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import AuthLayout from "./AuthLayout";
 import { Link } from "react-router-dom";
-import Input from "../../components/shared/small/Input";
-import { GoogleIcon } from "../../assets/svgs/Icon";
 import Button from "../../components/shared/small/Button";
+import Input from "../../components/shared/small/Input";
+import AuthLayout from "./AuthLayout";
+import { useForgetPasswordMutation } from "../../redux/apis/authApis";
+import { useState } from "react";
+import toast from "react-hot-toast";
 const ForgetPassword = () => {
+  const [email, setEmail] = useState("");
+  const [forgetPassword, { isLoading }] = useForgetPasswordMutation();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!email) return toast.error("Please Select Email");
+      const res = await forgetPassword({ email }).unwrap();
+      toast.success(res?.message);
+      setEmail("");
+    } catch (error) {
+      console.log("Error in forgetPassword:", error);
+      toast.error(error?.data?.message);
+    }
+  };
   return (
     <AuthLayout>
       <div className="h-full flex flex-col justify-around">
@@ -15,29 +30,25 @@ const ForgetPassword = () => {
           >
             Sign In
           </Link>
-          <Link
-            to="/register"
-            className="py-2 px-6 rounded-[10px] text-primary text-sm md:text-base"
-          >
+          <Link to="/register" className="py-2 px-6 rounded-[10px] text-primary text-sm md:text-base">
             Register
           </Link>
         </div>
         <div className="mt-4 md:mt-6">
-          <h6 className="text-lg md:text-2xl font-bold text-[#414141]">
-          Forgot Password 
-          </h6>
+          <h6 className="text-lg md:text-2xl font-bold text-[#414141]">Forgot Password</h6>
           <p className="my-4 text-sm text-[#071A17CC]">
             Enter your email and we will send you a link to reset your password
           </p>
-          <form className="mt-4 md:mt-6">
+          <form className="mt-4 md:mt-6" onSubmit={handleSubmit}>
             <div className="mb-4">
-              <Input type="email" placeholder="Enter Email" />
+              <Input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <Button
               type="submit"
               text="Continue"
               width="w-full"
               height="h-[45px] sm:h-[57px]"
+              className={`${isLoading && "pointer-events-none opacity-30 cursor-not-allowed"}`}
             />
           </form>
         </div>
--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../../components/shared/small/Button";
-import { GoogleIcon } from "../../assets/svgs/Icon";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import AuthLayout from "./AuthLayout";
 import Input from "../../components/shared/small/Input";
 
 const ResetPassword = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] =
-    useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
-  const confirmPasswordVisibilityHandler = () => {
-    setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
-  };
-  const passwordVisibilityHandler = () => {
-    setIsPasswordVisible(!isPasswordVisible);
-  };
+  const confirmPasswordVisibilityHandler = () => setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
+  const passwordVisibilityHandler = () => setIsPasswordVisible(!isPasswordVisible);
   return (
     <AuthLayout>
       <div className="h-full flex flex-col justify-between">
@@ -27,45 +21,26 @@ const ResetPassword = () => {
           >
             Sign In
           </Link>
-          <Link
-            to="/register"
-            className="py-2 px-6 rounded-[10px] text-primary text-sm md:text-base"
-          >
+          <Link to="/register" className="py-2 px-6 rounded-[10px] text-primary text-sm md:text-base">
             Register
           </Link>
         </div>
         <div className="mt-4 md:mt-6">
-          <h6 className="text-lg md:text-2xl font-bold text-[#414141]">
-            Reset new password
-          </h6>
-          <p className="mt-4 text-sm text-[#071A17CC] text-center">
-            Enter New Password
-          </p>
+          <h6 className="text-lg md:text-2xl font-bold text-[#414141]">Reset new password</h6>
+          <p className="mt-4 text-sm text-[#071A17CC] text-center">Enter New Password</p>
           <form className="mt-4 md:mt-6">
             <div className="relative my-4">
-              <Input
-                type={isPasswordVisible ? "text" : "password"}
-                placeholder="Password"
-              />
+              <Input type={isPasswordVisible ? "text" : "password"} placeholder="Password" />
               <div className="absolute top-1/2 -translate-y-1/2 right-4">
                 {isPasswordVisible ? (
-                  <VscEye
-                    onClick={passwordVisibilityHandler}
-                    className="text-[#949BA5] text-lg cursor-pointer"
-                  />
+                  <VscEye onClick={passwordVisibilityHandler} className="text-[#949BA5] text-lg cursor-pointer" />
                 ) : (
-                  <VscEyeClosed
-                    onClick={passwordVisibilityHandler}
-                    className="text-[#949BA5] text-lg cursor-pointer"
-                  />
+                  <VscEyeClosed onClick={passwordVisibilityHandler} className="text-[#949BA5] text-lg cursor-pointer" />
                 )}
               </div>
             </div>
             <div className="relative mb-4">
-              <Input
-                type={isConfirmPasswordVisible ? "text" : "password"}
-                placeholder="Confirm Password"
-              />
+              <Input type={isConfirmPasswordVisible ? "text" : "password"} placeholder="Confirm Password" />
               <div className="absolute top-1/2 -translate-y-1/2 right-4">
                 {isConfirmPasswordVisible ? (
                   <VscEye
@@ -80,12 +55,7 @@ const ResetPassword = () => {
                 )}
               </div>
             </div>
-            <Button
-              type="submit"
-              text="Reset"
-              width="w-full"
-              height="h-[45px] sm:h-[57px]"
-            />
+            <Button type="submit" text="Reset" width="w-full" height="h-[45px] sm:h-[57px]" />
           </form>
         </div>
         <div className="mt-4 md:mt-8 flex justify-center gap-4">
