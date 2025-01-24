ddbf351c2f399a40097b5cf674aa4962f6e5ffe2
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-01-24 15:23:35 +0500

    uncomennted protected

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -3,10 +3,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoutes = ({ requiresAuth = false, redirect = "/login", role }) => {
-  // const { user } = useSelector((state) => state.auth);
-  // if (requiresAuth && !user) return <Navigate to={redirect} replace />;
-  // if (requiresAuth && user?.role != role) return <Navigate to={`/${user?.role}`} replace />;
-  // if (!requiresAuth && user) return <Navigate to={`/${user?.role}`} replace />;
+  const { user } = useSelector((state) => state.auth);
+  if (requiresAuth && !user) return <Navigate to={redirect} replace />;
+  if (requiresAuth && user?.role != role) return <Navigate to={`/${user?.role}`} replace />;
+  if (!requiresAuth && user) return <Navigate to={`/${user?.role}`} replace />;
   return <Outlet />;
 };
 
