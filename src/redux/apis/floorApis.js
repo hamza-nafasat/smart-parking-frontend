2e986c3e9c5ea32f8371b303937a41928310a1ae
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-29 19:23:21 +0500

    add floor apis

---
+++ src/redux/apis/floorApis.js
@@ -0,0 +1,56 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import getEnv from "../../configs/config.js";
+
+const floorApis = createApi({
+  reducerPath: "floorApis",
+  baseQuery: fetchBaseQuery({ baseUrl: `${getEnv("SERVER_URL")}/api/floor`, credentials: "include" }),
+
+  endpoints: (builder) => ({
+    // create new floor
+    createFloor: builder.mutation({
+      query: (data) => ({
+        url: "/create",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    // get all floors
+    getAllFloors: builder.query({
+      query: () => ({
+        url: "/all",
+        method: "GET",
+      }),
+    }),
+    // get single floor
+    getSingleFloor: builder.query({
+      query: (id) => ({
+        url: `/single/${id}`,
+        method: "GET",
+      }),
+    }),
+    // update single floor
+    updateSingleFloor: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/single/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    // delete single floor
+    deleteSingleFloor: builder.mutation({
+      query: (id) => ({
+        url: `/single/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateFloorMutation,
+  useGetAllFloorsQuery,
+  useGetSingleFloorQuery,
+  useUpdateSingleFloorMutation,
+  useDeleteSingleFloorMutation,
+} = floorApis;
+export default floorApis;
