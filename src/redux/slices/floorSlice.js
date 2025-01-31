b06ab6e947a68f08cf0b2feb0fd6a90a4de8e644
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-01-31 19:07:43 +0500

    update floor slice

--- src/redux/slices/floorSlice.js
+++ src/redux/slices/floorSlice.js
@@ -2,15 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const floorSlice = createSlice({
   name: "floor",
-  initialState: { floors: [] },
+  initialState: { floors: [], activeAccordionIndex: null },
   reducers: {
     addFloorsSample: (state, action) => {
-      state.floors = Array.from({ length: action.payload }, (_, index) => ({
-        floorNumber: index + 1, 
-      }));
+      const currentLength = state.floors.length;
+      const newLength = action.payload;
+      if (newLength > currentLength) {
+        const additionalFloors = Array.from({ length: newLength - currentLength }, (_, index) => ({
+          floorNumber: currentLength + index + 1,
+        }));
+        state.floors.push(...additionalFloors);
+      } else if (newLength < currentLength) {
+        state.floors = state.floors.slice(0, newLength);
+      }
     },
     addFloor: (state, action) => {
-      state.floors = state.floors.map((floor)=>floor.floorNumber == action.payload.floorNumber ? action.payload : floor);
+      state.floors = state.floors.map((floor) =>
+        floor.floorNumber == action.payload.floorNumber ? action.payload : floor
+      );
     },
     removeFloor: (state, action) => {
       state.floors = state.floors.filter((floor) => floor.floorNumber == action.payload);
@@ -18,8 +27,11 @@ const floorSlice = createSlice({
     resetFloors: (state) => {
       state.floors = [];
     },
+    setActiveAccordionIndex: (state, action) => {
+      state.activeAccordionIndex = action.payload;
+    },
   },
 });
 
-export const { addFloor,addFloorsSample, removeFloor, resetFloors } = floorSlice.actions;
+export const { addFloor, addFloorsSample, removeFloor, resetFloors, setActiveAccordionIndex } = floorSlice.actions;
 export default floorSlice;
