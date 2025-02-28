eaa4038ebaa69e86ae5a1c1751c5944359ff22c5
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-02-28 12:15:47 +0500

    FIX PRETTIRE

--- src/pages/admin/parkingSummary/ParkingSummary.jsx
+++ src/pages/admin/parkingSummary/ParkingSummary.jsx
@@ -1,13 +1,6 @@
-import React from "react";
-import {
-  parkingBookingData,
-  parkingSummaryColumns,
-} from "./components/ParkingSummaryColumns";
-import { tableStyles } from "../../../components/shared/small/dataTableStyles";
-import { CiSearch } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
-import DataTable from "react-data-table-component";
 import GlobalTable from "../../../components/shared/large/GlobalTable";
+import { parkingBookingData, parkingSummaryColumns } from "./components/ParkingSummaryColumns";
 
 const ParkingSummary = () => {
   const navigate = useNavigate();
