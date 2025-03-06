f9fc0ea7dc599a13fb5f82e2251e0865448f9d20
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-03-06 16:51:45 +0500

    add

---
+++ src/redux/apis/bookingApis.js
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import getEnv from '../../configs/config.js';
+
+const bookingApis = createApi({
+  reducerPath: 'bookingApis',
+  baseQuery: fetchBaseQuery({ baseUrl: `${getEnv('SERVER_URL')}/api/booking`, credentials: 'include' }),
+
+  endpoints: (builder) => ({
+    // create new booking
+    createBooking: builder.mutation({
+      query: (data) => ({
+        url: '/create',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: [{ type: 'Booking', id: 'LIST' }],
+    }),
+    // get all bookings
+    getAllBookings: builder.query({
+      query: () => ({
+        url: '/all',
+        method: 'GET',
+      }),
+      providesTags: [{ type: 'Booking', id: 'LIST' }],
+    }),
+    // get single booking
+    getSingleBooking: builder.query({
+      query: (id) => ({
+        url: `/single/${id}`,
+        method: 'GET',
+      }),
+    }),
+    // update single booking
+    updateSingleBooking: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/single/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: [{ type: 'Booking', id: 'LIST' }],
+    }),
+    // delete single booking
+    deleteSingleBooking: builder.mutation({
+      query: (id) => ({
+        url: `/single/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: [{ type: 'Booking', id: 'LIST' }],
+    }),
+  }),
+});
+
+export const {
+  useCreateBookingMutation,
+  useGetAllBookingQuery,
+  useGetSingleBookingQuery,
+  useUpdateSingleBookingMutation,
+  useDeleteSingleBookingMutation,
+} = bookingApis;
+export default bookingApis;
