cf16ba1e6fdef59b5e7274bc534edd9077483baa
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-03-07 17:07:20 +0500

    add

--- src/pages/user/bookingSummary/components/BookingList.jsx
+++ src/pages/user/bookingSummary/components/BookingList.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
-import { buildingsManagersListData } from "../../../../pages/admin/buildingInfo/utils/buildingData";
-import {
-  Calender,
-  FreeSpaceIcon,
-  HouseIcon,
-  LocationIcon,
-  OccupiedParkingIcon,
-  RedDelete,
-  SensorIssueIcon,
-  Time,
-  TotalParkingIcon,
-  TwentyFourSevenIcon,
-} from "../../../../assets/svgs/Icon";
-import { useNavigate } from "react-router-dom";
-import ReactPaginate from "react-paginate";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
-import Button from "../../../../components/shared/small/Button";
-import { GoDotFill } from "react-icons/go";
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
+import { GoDotFill } from 'react-icons/go';
+import ReactPaginate from 'react-paginate';
+import { useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
+import { Calender, HouseIcon, RedDelete, Time, TotalParkingIcon } from '../../../../assets/svgs/Icon';
+import Button from '../../../../components/shared/small/Button';
+import { useGetAllBookingsQuery } from '../../../../redux/apis/bookingApis';
+import { dateFormate, timeFormate } from '../../../../utils/features';
 
 // Carousel settings for image slider
 const carouselSettings = {
@@ -33,22 +24,18 @@ const carouselSettings = {
 
 // Main component
 const BookingList = ({ listData }) => {
-  const [currentPage, setCurrentPage] = useState(0); // current page state
-  const itemsPerPage = 4; // number of items per page
+  const { data, isLoading } = useGetAllBookingsQuery();
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 4;
 
   // Calculate the buildings to display based on the current page
   const offset = currentPage * itemsPerPage;
-  const currentBuildings = listData.slice(offset, offset + itemsPerPage);
-
-  // Handler for page change
-  const handlePageClick = (event) => {
-    setCurrentPage(event.selected);
-  };
+  const currentBuildings = data?.data?.slice(offset, offset + itemsPerPage);
 
   return (
     <div>
-      {currentBuildings.map((data, index) => (
-        <SingleBuilding key={index} data={data} />
+      {currentBuildings?.map((booking, index) => (
+        <SingleBuilding key={index} booking={booking} />
       ))}
 
       <ReactPaginate
@@ -62,20 +49,20 @@ const BookingList = ({ listData }) => {
             <FaChevronRight fontSize={12} />
           </button>
         }
-        breakLabel={"..."}
+        breakLabel={'...'}
         pageCount={Math.ceil(listData.length / itemsPerPage)}
         marginPagesDisplayed={2}
         pageRangeDisplayed={3}
-        onPageChange={handlePageClick}
-        containerClassName={"flex justify-end px-0 md:px-10 mt-4 space-x-4"}
-        pageClassName={"inline-block"}
+        onPageChange={() => setCurrentPage(event.selected)}
+        containerClassName={'flex justify-end px-0 md:px-10 mt-4 space-x-4'}
+        pageClassName={'inline-block'}
         pageLinkClassName={
-          "flex justify-center items-center h-[30px] w-[30px]  border border-gray-300 text-[#404B52] rounded hover:bg-gray-100 transition"
+          'flex justify-center items-center h-[30px] w-[30px]  border border-gray-300 text-[#404B52] rounded hover:bg-gray-100 transition'
         }
-        activeClassName={"bg-primary rounded  text-white"}
-        previousClassName={"inline-block"}
-        nextClassName={"inline-block"}
-        disabledClassName={"opacity-50 cursor-not-allowed"}
+        activeClassName={'bg-primary rounded  text-white'}
+        previousClassName={'inline-block'}
+        nextClassName={'inline-block'}
+        disabledClassName={'opacity-50 cursor-not-allowed'}
       />
     </div>
   );
@@ -84,7 +71,7 @@ const BookingList = ({ listData }) => {
 export default BookingList;
 
 // Single building component
-const SingleBuilding = ({ data }) => {
+const SingleBuilding = ({ booking }) => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-wrap justify-between gap-4 md:gap-6 building-slider border-b border-[#00000037] py-4">
@@ -92,14 +79,11 @@ const SingleBuilding = ({ data }) => {
         {/* Image carousel */}
         <div className="w-[249px] h-[150px] rounded-xl overflow-hidden">
           <Slider {...carouselSettings}>
-            {data?.buildingImages?.map((img, i) => (
-              <img
-                key={i}
-                src={img}
-                alt="image"
-                className="w-[249px] h-[150px] rounded-xl object-cover"
-              />
-            ))}
+            <img
+              src={booking?.building?.twoDImage?.url}
+              alt="building Image"
+              className="w-[249px] h-[150px] rounded-xl object-cover"
+            />
           </Slider>
         </div>
 
@@ -107,24 +91,22 @@ const SingleBuilding = ({ data }) => {
         <div className="flex flex-col justify-between">
           <div>
             <div className="flex gap-3 items-center mb-1">
-              <h4 className="text-base md:text-xl font-bold text-[#000]">
-                {data?.buildingName}
-              </h4>
+              <h4 className="text-base md:text-xl font-bold text-[#000]">{booking?.building?.name}</h4>
               <h3
                 className={`flex items-center text-xs font-bold capitalize border-[1px] px-2 py-1 rounded-full ${
-                  data.status === "active"
-                    ? "text-primary border-primary"
-                    : data.status === "completed"
-                    ? "text-[#015948] border-[#015948]"
-                    : data.status === "cancelled"
-                    ? "text-[#AD1702] border-[#AD1702]"
-                    : ""
+                  booking.status === 'active'
+                    ? 'text-primary border-primary'
+                    : booking.status === 'completed'
+                    ? 'text-[#015948] border-[#015948]'
+                    : booking.status === 'cancelled'
+                    ? 'text-[#AD1702] border-[#AD1702]'
+                    : ''
                 }`}
               >
                 <GoDotFill />
-                {data?.status}
+                {booking?.status}
               </h3>
-              {data.status == "active" && (
+              {booking.status == 'active' && (
                 <div className="cursor-pointer p-1 flex justify-center items-center bg-[#FF474F14] rounded-full border-[1px] border-[#FF474F]">
                   <RedDelete />
                 </div>
@@ -132,38 +114,36 @@ const SingleBuilding = ({ data }) => {
             </div>
             <div className="flex items-center gap-2">
               <HouseIcon />
-              <p className="text-sm font-semibold text-[#47484b]">
-                {data?.address}
-              </p>
+              <p className="text-sm font-semibold text-[#47484b]">{booking?.building?.address}</p>
             </div>
           </div>
 
           <div className="flex flex-wrap gap-4 xl:gap-9 mt-6">
             <ParkingList
               data={{
-                title: "Parking Spot",
-                value: "B-13",
+                title: 'Parking Spot',
+                value: booking?.slot?.id,
                 icon: <TotalParkingIcon />,
               }}
             />
             <ParkingList
               data={{
-                title: "Date",
-                value: "10/07/2024",
+                title: 'Date',
+                value: `${dateFormate(booking?.startTime)} - ${dateFormate(booking?.endTime)}`,
                 icon: <Calender />,
               }}
             />
             <ParkingList
               data={{
-                title: "Start Time",
-                value: " 06:30 pm",
+                title: 'Start Time',
+                value: new Date(booking?.startTime)?.toLocaleTimeString(),
                 icon: <Time />,
               }}
             />
             <ParkingList
               data={{
-                title: "End Time",
-                value: "11:00 am",
+                title: 'End Time',
+                value: new Date(booking?.endTime)?.toLocaleTimeString(),
                 icon: <Time />,
               }}
             />
@@ -189,12 +169,8 @@ const ParkingList = ({ data }) => {
     <div className="flex items-center gap-2">
       {data?.icon}
       <div>
-        <h4 className="text-sm md:text-base font-bold text-[#292D32]">
-          {data?.title}
-        </h4>
-        <h6 className="text-sm md:text-base font-medium text-[#000000CC]">
-          {data?.value}
-        </h6>
+        <h4 className="text-sm md:text-base font-bold text-[#292D32]">{data?.title}</h4>
+        <h6 className="text-sm md:text-base font-medium text-[#000000CC]">{data?.value}</h6>
       </div>
     </div>
   );
