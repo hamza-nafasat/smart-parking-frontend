a759d334512dc913d2842cd65263b805a7598976
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-03-13 16:59:36 +0500

    add

--- src/pages/user/dashboard/components/CustomTextfield.jsx
+++ src/pages/user/dashboard/components/CustomTextfield.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 
-const CustomTextfield = ({ type = 'text', placeholder, intext, icon, onChange }) => {
+const CustomTextfield = ({ type = 'text', placeholder, intext, icon, onChange, ...rest }) => {
   return (
     <div className="flex items-center gap-1 bg-[#F9FBFF] border border-[#e7e7e7] rounded-[10px] py-2 px-4 w-full">
       {/* <{CiSearch} fontSize={20} color="#7E7E7E" /> */}
       {icon}
       <p className="text-xs font-bold md:text-base text-[#7E7E7E] text-nowrap">{intext}</p>
       <input
+        {...rest}
         type={type}
         onChange={onChange}
         placeholder={placeholder}
