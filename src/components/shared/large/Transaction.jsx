c8e783ccbf64e41f4845e359c3b490f4b6ea8e2b
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-03-28 15:16:09 +0500

    add all data

--- src/components/shared/large/Transaction.jsx
+++ src/components/shared/large/Transaction.jsx
@@ -1,17 +1,13 @@
 /* eslint-disable react/prop-types */
+import { TransactionsIcon } from '../../../assets/svgs/Icon';
 const Transaction = ({ transaction }) => {
-  // const maskCardNumber = (cardNumber) => {
-  //   return cardNumber.slice(0, 7) + '*'.repeat(cardNumber.length - 7);
-  // };
   return (
     <div className="flex flex-col sm:flex-row flex-wrap sm:items-center justify-between gap-4 py-2 border-b border-[#e5e5e5]">
       <div className="flex items-center gap-2">
-        <img src="https://placehold.co/45x45" alt="image" className="size-[48px] rounded-full object-cover" />
+        <TransactionsIcon className="size-[48px]" />
         <div>
-          <h6 className="text-sm md:text-base font-semibold text-[#414141] leading-none">Account title</h6>
-          <h6 className="text-xs text-[#414141] font-semibold leading-none">
-            {/* {maskCardNumber(transaction?.accountNumber)} */}
-          </h6>
+          <h6 className="text-sm md:text-base font-semibold text-[#414141] leading-none">Transaction Id</h6>
+          <h6 className="text-xs text-[#414141] font-semibold leading-none">{transaction?.id}</h6>
           <p className="text-[8px] text-[#41414199] leading-none mt-[2px]">{transaction?.date}</p>
         </div>
       </div>
