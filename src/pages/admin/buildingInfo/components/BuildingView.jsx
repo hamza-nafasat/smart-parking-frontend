f7539d9aada5e7c32b83401622fc5997bd5dcaad
Asad Noor <asadsipra2002@gmail.com>
2025-09-10 18:50:11 +0500

    building view and floor view admin flow analytics apis

--- src/pages/admin/buildingInfo/components/BuildingView.jsx
+++ src/pages/admin/buildingInfo/components/BuildingView.jsx
@@ -87,7 +87,7 @@ const BuildingView = () => {
     }
   }, [data, bookingSummaryData, buildingFloors]);
 
-  // // // // // // // // //----------------------------------
+  // // // // // // // // //
   return (
     <div className="grid grid-cols-12 gap-4">
       <div className="col-span-12 lg:col-span-3 bg-red-600 flex">
